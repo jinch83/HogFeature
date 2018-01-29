badc671294a6cbbbaa559589da7408febe7f2c3e
徐燦振 <jinch83@josanburi-no-MacBook-Air.local>
2018-01-29 16:55:46 +0900

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "hog.hpp"
 
 int main(){
-  Mat img = imread("/Users/jinch83/Desktop/data/a/a001.png");
+  Mat img = imread("~/Desktop/data/a/a001.png");
 
   HogFeature hog;
   hog.getFeature(img);
